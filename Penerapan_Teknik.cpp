003e5bdac356ac9862936f17b1378c6d6a3ddda0
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2017-12-30 01:00:38 +0800

    Menampilkan nilai dari MATRIKS X dengan DO WHILE

--- Penerapan_Teknik.cpp
+++ Penerapan_Teknik.cpp
@@ -31,4 +31,10 @@ int main(){
 	RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4],
 	RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4]};
 	
+	cout << "MATRIKS X : "<< endl;
+	
+	int a=0;
+		do{
+		cout << a1c[a]<< " | ";	a++;}
+		while (a<5);cout << endl;
 }
